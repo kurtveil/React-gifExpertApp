6f38b09f40cbe7186ab2e7f21b237e4738c94acb
Kurt <kurtveil1@gmail.com>
2021-11-10 16:42:41 -0500

    second commit

--- src/Components/GridGif.js
+++ src/Components/GridGif.js
@@ -8,6 +8,7 @@ export const GridGif = ({ category }) => {
 
   return (
     <>
+    
       <h3 className="animate__animated animate__fadeIn">{category}</h3>
       {loading && <p className="animate__animated animate__flash">Loading</p>}
       <div className="card-grid">
